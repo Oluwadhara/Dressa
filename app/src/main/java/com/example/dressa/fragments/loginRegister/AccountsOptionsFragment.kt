f20cc158247bd843ad22b461360a19d2d6c1edbb
Oluwadhara <olufemioluwadara@gmail.com>
2024-04-23 12:23:19 +0100

    initial commit

---
+++ app/src/main/java/com/example/dressa/fragments/loginRegister/AccountsOptionsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.dressa.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.dressa.R
+
+class AccountsOptionsFragment: Fragment(R.layout.fragment_account_options) {
+}
---
+++ app/src/main/java/com/example/dressa/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package com.example.dressa.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.dressa.R
+
+class RegisterFragment: Fragment(R.layout.fragment_register) {
+}
